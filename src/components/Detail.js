d8f7cf4044e6a96ac09f58049119eeab58ea9235
JTMasta <j.jthyriar@gmail.com>
2022-10-16 21:18:42 -0400

    Cleaning up component and editing paragraph

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Typography, Stack } from '@mui/material'
 
-const Detail = () => {
+
+const Detail = ({ exerciseDetail }) => {
+  const { bodyPart, gifUrl, name, target } = exerciseDetail
+
   return (
-    <div>Detail</div>
+    <Stack gap="60px" sx={{ flexDirection: { lg: 'row'}, p: '20px', alignItems: 'center' }}>
+      <img src={gifUrl} alt={name} loading="lazy" className='detail-image'/>
+      <Stack sx={{ gap: { lg: '35px', xs: '20px' }}}>
+        <Typography variant="h3" sx={{ textTransform: 'capitalize'}}>
+          {name}
+        </Typography>
+        <Typography variant="h6">
+          The {name} {` `}
+
+          works on the <strong>{bodyPart}</strong> portion of your body and specifically targets your <strong>{target}</strong>. Great workout to improve your overall physique!
+        </Typography>
+      </Stack>
+    </Stack>
   )
 }
 
